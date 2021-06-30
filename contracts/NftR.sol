7a1be70b3ca5ed1215a5626f71f609dfeba2228f
Anupam Kumar <atiriktmail@gmail.com>
2021-06-30 23:14:01 +0530

    added minter and tests

--- contracts/NftR.sol
+++ contracts/NftR.sol
@@ -4,7 +4,31 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NftR is ERC721 {
 
-  constructor() ERC721("Rnft", "RNFT") public {
+  address public minter;
+  address payable public admin;
+  string[] public links;
+  mapping(string=>bool) public linkGenerated;
+  uint256 public totalSupply = 0;
 
+  constructor() ERC721("NftR", "NftR") public {
+    admin = payable(msg.sender);
+    minter = admin;
   }
+
+  function setMinter(address _minter) public{
+    require(msg.sender == admin, 'address not admin');
+    minter = _minter;
+  }
+
+  function mint(string memory _link, address _to) public {
+    require(msg.sender == minter, 'address not minter');
+    require(linkGenerated[_link] == false, 'link taken');
+    links.push(_link);
+    uint256 _id = links.length;
+    _safeMint(_to, _id);
+    linkGenerated[_link] = true;
+    totalSupply++;
+  }
+
+
 }
--- test/NfTrTest.js
+++ test/NfTrTest.js
@@ -2,6 +2,7 @@ const NftR = artifacts.require("./NftR.sol")
 
 contract('NftR', function(accounts){
   var nftObj;
+
   it('deploya successfully', function(){
     return NftR.deployed().then(function(obj){
       nftObj = obj;
@@ -9,6 +10,45 @@ contract('NftR', function(accounts){
     }).then(function(address){
       console.log(address);
       assert.notEqual(address, null, "address ok");
+      return nftObj.name();
+    }).then(function(name){
+      assert.equal(name, 'NftR');
+      return nftObj.symbol();
+    }).then(function(sym){
+      assert.equal(sym, 'NftR');
     })
+  })
+
+  it('minting', function(){
+    var string = "https://HttpIsCentralized.com";
+    return NftR.deployed().then(function(obj){
+      nftObj = obj;
+      return nftObj.minter();
+    }).then(function(minter){
+      assert.equal(minter, accounts[0],'minter ok');
+      return nftObj.admin();
+    }).then(function(admin){
+      assert.equal(admin, accounts[0], 'admin ok');
+      return nftObj.mint(string , accounts[1], {from:accounts[0]});
+    }).then(function(receipt){
+      //console.log(receipt.logs[0].args);
+      assert.equal(receipt.logs.length, 1, 'log length ok');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'event ok');
+      assert.equal(receipt.logs[0].args.to, accounts[1], 'receipt receiver ok');
+      return nftObj.linkGenerated(string);
+    }).then(function(ret){
+      assert.equal(ret, true);
+      return nftObj.totalSupply();
+    }).then(function(supply){
+      assert.equal(supply, 1, 'total supply ok');
+      return nftObj.mint(string , accounts[1], {from:accounts[0]});
+    }).then(assert.fail).catch(async function(error){
+      //console.log(error.message);
+      assert(error.message.indexOf('revert')>=0,'same token revert ok');
+      for(var i = 0; i <= nftObj.links.length; i++){
+        let link = await nftObj.links(i);
+        assert(link.indexOf('Http')>=0); 
+      }
+   })
   })
 })
